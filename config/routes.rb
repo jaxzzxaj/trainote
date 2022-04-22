06be298b90d8cc5894f87d63f1412b1c45d9dde7
jaxzzxaj <programyourlife1020@gmail.com>
2022-04-22 16:47:40 +0900

    メモの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #"/"のリクエストをされたときに行動するアクションを設定。
   root to: 'manages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
