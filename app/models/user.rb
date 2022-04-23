31b2942a4e6bccf7ab8b5f68dd87d7e7a1a0b7ec
jaxzzxaj <programyourlife1020@gmail.com>
2022-04-23 14:59:06 +0900

    パスワードの正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,16 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password
     validates :email
   end
 
+  #新規登録時のパスワードの設定
+  with_options on: :create do
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message:"は半角英数を両方含む必要があります"}
+  end
+
+  #アカウント編集のパスワードの設定
+  with_options on: :update do
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message:"は半角英数を両方含む必要があります"}
+  end
 end
