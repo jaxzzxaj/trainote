7726e12d20c9becd618cf71cee8a9c0d87ee73df
jaxzzxaj <programyourlife1020@gmail.com>
2022-04-23 14:15:41 +0900

    アカウント編集機能
    １）viewで編集画面へ遷移する設定を追記
    ２）application_controllerにacount_updateするとき保存するものを指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
   # deviseではemail,password, password_confirmationはデフォルトで入ってる！
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:acount_update, keys:[:nickname])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  #devise_for :usersはユーザーの登録やログイン、ログアウトの機能は実装されている。
   devise_for :users
   #"/"のリクエストをされたときに行動するアクションを設定。
   root to: 'manages#index'
 
+  #テーブルに対して、許可するアクションの設定。
 end
